77e6746e164e34853748ed30159337f2aa518eb6
Nik <38110784+jjjamer@users.noreply.github.com>
2018-05-02 22:16:43 -0500

    Current Collab Stuff, Attempt #2

--- The Alchemist/Assets/Scripts/dragonFollow.cs
+++ The Alchemist/Assets/Scripts/dragonFollow.cs
@@ -81,6 +81,7 @@ public class dragonFollow : MonoBehaviour {
         //Debug.Log(speed);
 
         grounded = Physics2D.OverlapCircle(dragonGroundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Grounded", grounded);
 
         if (Vector3.Distance(transform.position, dragonTarget.position) > distanceFromPlayer){
             transform.position = Vector3.MoveTowards(transform.position, dragonTarget.position, step);
